e711e963f38e6a5461492c8b430e6e55a75c2ab6
Dan Søndergaard <dan1990@gmail.com>
2013-08-14 11:08:41 +0200

    PEP8.

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ env.use_ssh_config = True
 
 env.hosts = ['login.genome.au.dk']
 
+
 @task
 def setup():
     local('python setup.py sdist')
@@ -16,6 +17,7 @@ def setup():
     run('tar xf gwf-0.1.0.tar.gz')
     with cd('gwf-0.1.0'):
         run('python setup.py install --force --user')
+
 
 @task
 def test_workflow_gwf():
