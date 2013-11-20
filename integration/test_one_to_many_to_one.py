9a642ddbbc495e14b7894bd56e21b7265191480e
Dan Søndergaard <dan1990@gmail.com>
2013-11-20 13:36:38 +0100

    Add integration test.

---
+++ integration/test_one_to_many_to_one.py
@@ -0,0 +1,17 @@
+import os.path
+import unittest
+
+from integration import IntegrationTestCase, Sandbox
+
+
+class OneToManyToOneTest(IntegrationTestCase):
+
+    requirements = ['test_one_to_many_to_one.gwf']
+
+    def runTest(self):
+        with Sandbox(self.requirements) as s:
+            s.run('gwf -l -f test_one_to_many_to_one.gwf')
+            self.assertFileExists(os.path.join(s.sandbox_path, 'final'))
+
+if __name__ == '__main__':
+    unittest.main()
