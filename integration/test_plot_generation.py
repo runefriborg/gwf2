b56c7ec0eec82883e67992be190e1c03eeae85b4
Dan Søndergaard <dan1990@gmail.com>
2013-11-27 11:50:25 +0100

    Fix plot generation test.

--- integration/test_plot_generation.py
+++ integration/test_plot_generation.py
@@ -2,19 +2,18 @@ import os
 import os.path
 import unittest
 
-from .integration import IntegrationTestCase, Sandbox
+from integration import IntegrationTestCase, Sandbox
 
 
 class PlotGenerationTest(IntegrationTestCase):
 
-    requirements = ['test_plot_generation.gwf']
+    requirements = ['test_plot_generation.gwf', 'ponAbe2.fa.gz']
 
     def runTest(self):
         with Sandbox(self.requirements) as s:
             s.run('gwf -l -f test_plot_generation.gwf')
 
             job_id = os.listdir(os.path.join(s.sandbox_config_path, 'jobs'))[0]
-            s.run('tree {0}'.format(os.path.join(s.sandbox_path)))
 
             s.run('gwf-status -j {0} -p timeline'.format(job_id))
             self.assertFileExists(os.path.join(s.sandbox_path,
