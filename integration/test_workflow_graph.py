1d4c2315e75cd75594b9a73c37125f4f35e2d07d
Dan Søndergaard <dan1990@gmail.com>
2014-01-06 12:59:10 +0100

    Fix workflow graph test.

--- integration/test_workflow_graph.py
+++ integration/test_workflow_graph.py
@@ -12,7 +12,9 @@ class WorkflowGraphTest(IntegrationTestCase):
     def runTest(self):
         with Sandbox(self.requirements) as s:
             s.run('gwf-workflow-graph test_one_target_of_multiple.gwf')
-            self.assertFileExists(os.path.join(s.sandbox_path, 'workflow.dot'))
+            self.assertFileExists(os.path.join(s.sandbox_path, 'test_one_target_of_multiple.gwf.dot'))
+            s.run('dot -Tpng test_one_target_of_multiple.gwf.dot > test.png')
+            self.assertFileExists(os.path.join(s.sandbox_path, 'test.png'))
 
 if __name__ == '__main__':
     unittest.main()
