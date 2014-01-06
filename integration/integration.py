0f022b954fedc499ec204da61b1f310c222cdf21
Dan Søndergaard <dan1990@gmail.com>
2014-01-06 12:17:22 +0100

    Fix env variable name.

--- integration/integration.py
+++ integration/integration.py
@@ -37,7 +37,7 @@ class Sandbox(object):
 
     def run(self, command):
         sandbox_environment = dict(os.environ)
-        sandbox_environment['GWF_SCRATCH_DIR'] = self.sandbox_scratch_path
+        sandbox_environment['GWF_SCRATCH'] = self.sandbox_scratch_path
         sandbox_environment['GWF_CONFIG_DIR'] = self.sandbox_config_path
         sandbox_environment['GWF_DEBUG'] = ''
 
