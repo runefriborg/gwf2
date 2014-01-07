65dcc209bc6f85f2456d77be98651abb53c93c85
Dan Søndergaard <dan1990@gmail.com>
2014-01-07 14:18:12 +0100

    Implement wait function to wait for a qsubbed job to finish.

--- integration/test_submit_workflow.py
+++ integration/test_submit_workflow.py
@@ -5,12 +5,18 @@ from integration import IntegrationTestCase
 from gwf.process import local
 
 
+def wait(command):
+    out = local(command)
+    ids = [line for line in out.splitlines() if line.endswith('.in')]
+    local('qwait ' + ' '.join(ids))
+
+
 class SubmitLinearWorkflowTest(IntegrationTestCase):
 
     requirements = ['test_chain_of_checkpointed_targets.gwf']
 
     def runTest(self):
-        local('gwf test_chain_of_checkpointed_targets.gwf')
+        wait('gwf test_chain_of_checkpointed_targets.gwf')
         self.assertNotFileExists('file_one')
         self.assertFileExists('file_two')
         self.assertNotFileExists('file_three')
@@ -23,7 +29,7 @@ class SubmitOneToManyToOneWorkflowTest(IntegrationTestCase):
     requirements = ['test_one_to_many_to_one.gwf']
 
     def runTest(self):
-        local('gwf test_one_to_many_to_one.gwf')
+        wait('gwf test_one_to_many_to_one.gwf')
         self.assertFileExists('final')
 
 if __name__ == '__main__':
