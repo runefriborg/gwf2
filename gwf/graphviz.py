b116e48231e3e893e3951d8295e93474e8d83dc5
runefriborg <rune.m.friborg@gmail.com>
2014-01-07 14:49:44 +0100

    Added grouping to gwf-workflow-graph

--- gwf/graphviz.py
+++ gwf/graphviz.py
@@ -25,27 +25,46 @@ def print_node(node, out):
 
     shape = 'shape = %s' % task_shape(node.task)
 
-    print >> out, '"%s"' % node.name,
+    print >> out, '\t"%s"' % node.name,
     print >> out, '[',
     print >> out, ','.join([shape, 'color = darkgreen']),
     print >> out, ']',
     print >> out, ';'
 
+def print_group(group, out):
+    '''Print the graphviz description of this group to "out".'''
+    label = group.id + '___' + group.pos
+    print >> out, 'subgraph "cluster_%s" {' % label
+    print >> out, '\tlabel="%s";' % label
+    for node in group.nodes:
+        print_node(node, out)
+    print >> out, '}'
 
-def print_graphviz(graph, out):
-    '''Print the dependency graph, graph, to output stream out.'''
+def print_graphviz(graph, tasklist, groups, hide_boxes, out):
+    '''Print the dependency graph in groups, to output stream out.'''
 
     print >> out, 'digraph workflow {'
 
     # Handle nodes
-    for node in graph.nodes.values():
-        print_node(node, out)
+    if hide_boxes:
+        for g in groups:
+            for n in g.nodes:
+                print_node(n, out)
+    else:
+        for g in groups:
+            if g.nodes:
+                print_group(g, out)
 
     # Handle edges
     for src in graph.nodes.values():
+
+        # Skip graph node if not part of dependency path to specified targets
+        if src.task not in tasklist:
+            continue
+
         for fname, dst in src.dependencies:
             print >> out, '"%s"' % dst.name, '->', '"%s"' % src.name,
-            print >> out, '[label="%s"]' % fname,
+            print >> out, '[label="%s"]' % (fname),
             print >> out, ';'
 
     print >> out, '}'
