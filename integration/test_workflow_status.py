f4d85cf5048e070b8ed0de6cb20785004f2208a1
Dan Søndergaard <dan1990@gmail.com>
2014-01-07 13:13:10 +0100

    Add test of gwf-status tool.

--- integration/test_workflow_status.py
+++ integration/test_workflow_status.py
@@ -4,29 +4,62 @@ import unittest
 from integration import IntegrationTestCase, Sandbox
 
 
-class OneTaskFailsTest(IntegrationTestCase):
+# class OneTaskFailsTest(IntegrationTestCase):
 
-    requirements = ['test_one_task_fails.gwf']
+#     requirements = ['test_one_task_fails.gwf']
+
+#     def runTest(self):
+#         with Sandbox(self.requirements) as s:
+#             s.run('gwf -l test_one_task_fails.gwf')
+#             self.assertTrue(s.run('gwf-status').find("failed") > -1)
+
+
+# class MiddleTaskFailsTest(IntegrationTestCase):
+
+#     requirements = ['test_middle_task_fails.gwf']
+
+#     def runTest(self):
+#         with Sandbox(self.requirements) as s:
+#             s.run('gwf -l test_middle_task_fails.gwf')
+
+#             self.assertTrue(s.run('gwf-status --no-color -j 00001').find("TargetOne   C") > -1)
+#             self.assertTrue(s.run('gwf-status --no-color -j 00001').find("TargetTwo   C") > -1)
+#             self.assertTrue(s.run('gwf-status --no-color -j 00001').find("TargetThree F") > -1)
+#             self.assertTrue(s.run('gwf-status --no-color -j 00001').find("TargetFour  F") > -1)
+#             self.assertTrue(s.run('gwf-status --no-color -j 00001').find("TargetFive  F") > -1)
+
+
+class StatusDuringRunTest(IntegrationTestCase):
+
+    requirements = ['test_task_two_of_three_fails.gwf']
 
     def runTest(self):
+        import time
+        import multiprocessing
+
         with Sandbox(self.requirements) as s:
-            s.run('gwf -l test_one_task_fails.gwf')
-            self.assertTrue(s.run('gwf-status | grep "failed"').find("failed"))
+            def test_status():
+                # wait for the first task to finish
+                while not os.path.exists(os.path.join(s.sandbox_scratch_path, '00001', 'TargetOne', 'file_one')):
+                    time.sleep(0.1)
 
+                # when the first task has produced file_one, we sleep a bit
+                # to let TargetTwo start
+                time.sleep(2)
 
-class MiddleTaskFailsTest(IntegrationTestCase):
+                # then check if the status of TargetTwo is "running"
+                self.assertTrue(s.run('gwf-status --no-color -j 00001').find('TargetTwo R') > -1)
 
-    requirements = ['test_middle_task_fails.gwf']
+                # wait 10 seconds. TargetTwo should then have failed and
+                # TargetThree should be running.
+                time.sleep(10)
+                self.assertTrue(s.run('gwf-status --no-color -j 00001').find('TargetOne C') > -1)
+                self.assertTrue(s.run('gwf-status --no-color -j 00001').find('TargetTwo F') > -1)
 
-    def runTest(self):
-        with Sandbox(self.requirements) as s:
-            s.run('gwf -l test_middle_task_fails.gwf')
-
-            self.assertTrue(s.run('gwf-status -j 00001').find("TargetOne   C"))
-            self.assertTrue(s.run('gwf-status -j 00001').find("TargetTwo   C"))
-            self.assertTrue(s.run('gwf-status -j 00001').find("TargetThree F"))
-            self.assertTrue(s.run('gwf-status -j 00001').find("TargetFour  F"))
-            self.assertTrue(s.run('gwf-status -j 00001').find("TargetFive  F"))
+            process = multiprocessing.Process(target=test_status, args=())
+            process.start()
+            s.run('gwf -l test_task_two_of_three_fails.gwf')
+            process.join()
 
 if __name__ == '__main__':
     unittest.main()
